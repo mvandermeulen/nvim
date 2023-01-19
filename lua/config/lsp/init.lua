fbd1af7507a39b99bc765843bd02d2d210e1ed33
Mark <mark@vdm.id.au>
2023-01-19 23:54:36 +1100

    - Update from deprecated

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -26,7 +26,7 @@ end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 -- capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local hs_version = vim.fn.system('hs -c _VERSION'):gsub('[\n\r]', '')
 local hs_path = vim.split(vim.fn.system('hs -c package.path'):gsub('[\n\r]', ''), ';')
