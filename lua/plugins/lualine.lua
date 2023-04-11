8e80243e581137e03d96d1389bad6577f4ed30ae
Mark <mark@vdm.id.au>
2023-04-12 06:02:26 +1000

    Cleanup lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -14,7 +14,6 @@ if not navic_status then
 	return
 end
 
-
 local hide_in_width = function()
   return vim.fn.winwidth(0) > 80
 end
@@ -116,44 +115,3 @@ lualine.setup {
   extensions = { 'nvim-tree', 'toggleterm', 'quickfix', 'symbols-outline' },
 }
 
---require("lualine").setup({
---	options = {
---		icons_enabled = true,
---		component_separators = { "", "" },
---		section_separators = { "", "" },
---		disabled_filetypes = {},
---		always_divide_middle = true,
---		--theme = 'tokyonight',
---	},
---	sections = {
---		lualine_a = { "mode" },
--- lualine_b = { { "b:gitsigns_head", icon = "" }, "diff" },
---		lualine_c = {
---			{
---				"filename",
---				file_status = true,
---				path = 1, -- show relativ path
---				shorting_target = 40,
---				symbols = { modified = "[]", readonly = " " },
---			},
---		},
---		lualine_x = {
---			{ "diagnostics", sources = { "nvim_diagnostic" } },
---			"encoding",
---			"fileformat",
---			"filetype",
---		},
---		lualine_y = { "progress" },
---		lualine_z = { "location" },
---	},
---	inactive_sections = {
---		lualine_a = {},
---		lualine_b = {},
---		lualine_c = { "filename" },
---		lualine_x = { "location" },
---		lualine_y = {},
---		lualine_z = {},
---	},
---	tabline = {},
---	extensions = { "nvim-tree", "toggleterm", "quickfix", "symbols-outline" },
---})
