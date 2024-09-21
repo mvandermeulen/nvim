2e5a0e61474b35cfd9d24714a0e57501a732fe59
Mark <mark@vdm.id.au>
2024-09-22 05:22:14 +1000

    Updated python install script

--- install_pytools.sh
+++ install_pytools.sh
@@ -14,11 +14,19 @@ apt-install() {
 
 pip-install() {
   if [ "$UBUNTU_RELEASE" = "jammy"]; then
-    pip install "$1"
+    pip install -r "$1"
   else
-    pip install --break-system-packages "$1"
+    pip install --break-system-packages -r "$1"
   fi
 }
+
+# pip-install() {
+#   if [ "$UBUNTU_RELEASE" = "jammy"]; then
+#     pip install "$1"
+#   else
+#     pip install --break-system-packages "$1"
+#   fi
+# }
 
 # directory is used by pipenv
 mkdir -p $HOME/.local/share/virtualenvs
@@ -29,32 +37,19 @@ mkdir -p $HOME/.cache/pypoetry
 # pre-commit directory for installing plugins
 mkdir -p $HOME/.cache/pre-commit
 
+CURRENT_DIR=$(pwd); VENV_PATH="${CURRENT_DIR}/venv"
+if [[ ! -d "${VENV_PATH}" ]]; then
+  echo "+ Could not find virtual environment in: ${CURRENT_DIR}"
+  cd "${CURRENT_DIR}" && python3.12 -m venv venv
+fi
+
+if [[ -d "${VENV_PATH}" ]]; then
+  source "${VENV_PATH}/bin/activate"
+  echo "+ Python virtual environment activated"
+fi
 
 
-# tldr for a short form man pages.
-pip-install tldr
+pip-install requirements.txt
 
-# Just gitgud
-pip-install gitgud
 
-pip-install mycli
-
-pip-install pgcli
-
-pip-install tomlkit
-
-pip-install iredis
-
-pip-install termcolor
-
-pip-install libtmux
-
-pip-install hiredis
-
-pip-install "redis[hiredis]"
-
-pip-install pre-commit
-
-pip-install virtualenvwrapper
-
-pip-install neovim-remote
+curl -LsSf https://astral.sh/ruff/install.sh | sh
