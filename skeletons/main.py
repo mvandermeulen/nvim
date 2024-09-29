31a8f040d3f37ed3af93b75c3570315d92a3fe9d
Mark <mark@vdm.id.au>
2024-09-30 01:43:39 +1000

    Added skeletons

---
+++ skeletons/main.py
@@ -0,0 +1,31 @@
+#!/Users/vandem/scripts/venv/bin/python3
+# encoding: utf-8
+"""
+
+name.py
+
+Created by Mark van der Meulen 2024.
+Copyright (c) 2024 Mark van der Meulen. All rights reserved.
+
+
+"""
+
+__author__ = "Mark van der Meulen"
+__status__ = "Testing/Unstable"
+
+# External Imports
+# import uuid, time, datetime
+# from icecream import ic
+
+# Internal Imports
+
+
+def main() -> None:
+  '''
+  Invoke this function as the entrypoint of the script.
+  '''
+  pass
+
+
+if __name__ == "__main__":
+  main()
---
+++ skeletons/zsh.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+##################
+##!/usr/bin/zsh
+##################
+
+# VARIABLES
+
+# FUNCTIONS
+
+# LOGIC
+
+
+exit 0
