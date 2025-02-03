21e71a5e9d0124dbba8335608ff19aac3737245c
Mark <mark@vdm.id.au>
2025-02-04 02:53:00 +0800

    Increase plugin manager concurrency
    This commit increases the plugin manager's concurrency setting from 6 to 8. This can potentially speed up plugin installation and updates.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -116,7 +116,7 @@ require('lazy').setup({ utility_plugins, file_plugins, lang_plugins, ui_plugins,
     missing = true,
     colorscheme = { 'default' },
   },
-  concurrency = 6,
+  concurrency = 8,
   git = {
     clone_timeout = 180,
     url_format = "git@github.com:%s.git",
