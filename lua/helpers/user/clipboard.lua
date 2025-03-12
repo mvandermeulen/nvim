a0b8a54a1e172e4b988a0d88c8de1aff840c6c82
Mark <mark@vdm.id.au>
2025-03-13 06:50:09 +0800

    Fix: Use vim.loop for socket connection instead of plenary.async
    The plenary.async library is no longer a hard dependency of the project. This commit replaces the use of `plenary.async` with `vim.loop` for socket connections, resolving potential dependency issues and aligning with core Neovim functionality.

--- lua/helpers/user/clipboard.lua
+++ lua/helpers/user/clipboard.lua
@@ -14,15 +14,21 @@ end
 
 local M = {}
 
-local async = require "plenary.async"
+-- local async = require "plenary.async"
 
 
 local function socket_paste()
   local socket_path = os.getenv('HOME') .. '/.local/tmp/snuggle.socket'
-  local socket = vim.uv.new_pipe(false)
-  local err = async.uv.pipe_connect(socket, vim.fn.resolve(socket_path))
-  assert(not err, err)
-  async.uv.write(socket, vim.fn.getreg(''))
+  local pipe = vim.loop.new_pipe(false)
+  vim.loop.pipe_connect(pipe, socket_path, function(err)
+      assert(not err, err)
+  end)
+  pipe:write(vim.fn.getreg(''))
+  pipe:close()
+  -- local socket = vim.uv.new_pipe(false)
+  -- local err = async.uv.pipe_connect(socket, vim.fn.resolve(socket_path))
+  -- assert(not err, err)
+  -- async.uv.write(socket, vim.fn.getreg(''))
 end
 
 function M.setup()
