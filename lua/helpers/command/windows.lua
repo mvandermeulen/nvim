84af1ec840d28e3c10c1f875ce5452651ff183f0
Mark <mark@vdm.id.au>
2025-06-25 16:21:53 +0800

    Added tmux helpers

--- lua/helpers/command/windows.lua
+++ lua/helpers/command/windows.lua
@@ -2,28 +2,36 @@
 -- Commands: Windows
 --
 -- Author: Mark van der Meulen
--- Updated: 2025-02-07
+-- Updated: 2025-06-17
 --]]
 
 
 vim.api.nvim_create_user_command("NewTabTiled", function()
-  vim.cmd("tabnew")
-  vim.cmd("new")
-  vim.cmd("vnew")
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-w>k', true, false, true), 'n', false)
-  vim.cmd("vnew")
+  vim.cmd.tabnew()
+  vim.cmd.vnew()
+  vim.cmd.new()
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-w>h', true, false, true), 'n', false)
+  vim.cmd.new()
 end, {})
 
 
 vim.api.nvim_create_user_command("NewTabHSplit", function()
-  vim.cmd("tabnew")
-  vim.cmd("new")
+  vim.cmd.tabnew()
+  vim.cmd.new()
 end, {})
 
 
 vim.api.nvim_create_user_command("NewTabVSplit", function()
-  vim.cmd("tabnew")
-  vim.cmd("vnew")
+  vim.cmd.tabnew()
+  vim.cmd.vnew()
+end, {})
+
+
+vim.api.nvim_create_user_command("NewTabTripleVSplit", function()
+  vim.cmd.tabnew()
+  vim.cmd.vnew()
+  vim.cmd.vnew()
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-w>=', true, false, true), 'n', false)
 end, {})
 
 -- tab buffer window list
