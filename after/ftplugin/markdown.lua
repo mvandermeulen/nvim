f5ed8aa63b6cc89d262a25e62cf7de60e08e287b
Mark <mark@vdm.id.au>
2025-07-16 01:41:04 +0800

    Update ftplugin for markdown

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,2 +1,2 @@
-vim.keymap.set("n", "<C-m>w", "<CMD>MakeWordMarkdownLink<CR>", { noremap = true, silent = true, desc = 'Make word Markdown Link' })
-vim.keymap.set("n", "<C-m>W", "<CMD>MakeNonWhitespaceWordMarkdownLink<CR>", { noremap = true, silent = true, desc = 'Make WORD Markdown Link' })
+-- vim.keymap.set("n", "<C-m>w", "<CMD>MakeWordMarkdownLink<CR>", { noremap = true, silent = true, desc = 'Make word Markdown Link' })
+-- vim.keymap.set("n", "<C-m>W", "<CMD>MakeNonWhitespaceWordMarkdownLink<CR>", { noremap = true, silent = true, desc = 'Make WORD Markdown Link' })
