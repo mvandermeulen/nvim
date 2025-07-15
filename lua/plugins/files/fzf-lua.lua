f474501136160632462c40a70972ce0647178675
Mark <mark@vdm.id.au>
2025-07-16 01:45:32 +0800

    Tidy fzf-lua

--- lua/plugins/files/fzf-lua.lua
+++ lua/plugins/files/fzf-lua.lua
@@ -9,103 +9,71 @@ if not status_ok then
   return
 end
 
-local icons = require('helpers.ui.icons')
 local fzf_config = require('helpers.plugins.fzf_config')
-local actions = require('fzf-lua.actions')
 
-fzf_lua.setup({
-  "borderless",
-  keymap = {
-    builtin = {
-      ["<M-Esc>"]     = "hide",     -- hide fzf-lua, `:FzfLua resume` to continue
-      ["<F1>"]        = "toggle-help",
-      ["<F2>"]        = "toggle-fullscreen",
-      ["<F3>"]        = "toggle-preview-wrap",
-      ["<F4>"]        = "toggle-preview",
-      ["<C-p>"] = "toggle-preview",
-      ['<M-f>'] = 'toggle-fullscreen',
-      ["<S-down>"]    = "preview-page-down",
-      ["<S-up>"]      = "preview-page-up",
-      -- ['<M-d>'] = 'preview-page-down',
-      -- ['<M-u>'] = 'preview-page-up',
-      -- ['<M-k>'] = 'first',
-      -- ['<M-j>'] = 'last',
-    },
-    fzf = {
-      ["ctrl-z"]      = "abort",
-      ["ctrl-u"]      = "unix-line-discard",
-      ['ctrl-r']      = 'select-all+accept',
-      ["ctrl-f"]      = "half-page-down",
-      ["ctrl-b"]      = "half-page-up",
-      ["ctrl-a"]      = "beginning-of-line",
-      ["ctrl-e"]      = "end-of-line",
-      ["alt-a"]       = "toggle-all",
-      ["alt-g"]       = "first",
-      ["alt-G"]       = "last",
-      -- Previews
-      ["f3"]          = "toggle-preview-wrap",
-      ["f4"]          = "toggle-preview",
-      -- ["ctrl-f"]      = "preview-page-down",
-      -- ["ctrl-b"]      = "preview-page-up",
-      ["shift-down"]  = "preview-page-down",
-      ["shift-up"]    = "preview-page-up",
-      -- ["tab"]         = "down",
-      -- ["shift-tab"]   = "up",
-    },
-  },
-  winopts = {
-    border = "none",
-    height = 0.65,
-    width = 0.70,
-    preview = {
-      border = "none",
-      layout = 'flex',
-      flip_columns = 180,
-      scrollbar = 'float',
-      scrolloff = "-2",
-      delay = 50,
-    },
-  },
-  git = {
-    icons = {
-      ["M"] = { icon = icons.git.Mod, color = "yellow" },
-      ["D"] = { icon = icons.git.RemoveAlt, color = "red" },
-      ["A"] = { icon = icons.git.Add, color = "green" },
-      ["R"] = { icon = icons.git.Rename, color = "yellow" },
-      ["C"] = { icon = icons.git.Mod, color = "yellow" },
-      ["T"] = { icon = icons.git.Mod, color = "magenta" },
-      ["?"] = { icon = icons.git.Untrack, color = "magenta" },
-    },
-  },
-  actions = {
-    files = {
-      ["enter"] = actions.file_edit_or_qf,
-      ['default'] = actions.file_edit_or_qf,
-      ['ctrl-t'] = actions.file_tabedit,
-      ['ctrl-x'] = actions.file_split,
-      ['ctrl-v'] = actions.file_vsplit,
-      ["alt-i"]  = actions.toggle_ignore,
-      ["alt-h"]  = actions.toggle_hidden,
-      ["alt-f"]  = actions.toggle_follow,
-    },
-    buffers = {
-      ['default'] = actions.buf_edit,
-      ['ctrl-x'] = actions.buf_split,
-      ['ctrl-v'] = actions.buf_vsplit,
-      ['ctrl-t'] = actions.buf_tabedit,
-    },
-  },
-  oldfiles = fzf_config.oldfiles,
-  grep = fzf_config.grep,
-  buffers = fzf_config.buffers,
-  tabs = fzf_config.tabs,
-  files = fzf_config.files,
-})
 
+--------------------------------------
+---------- Helper Functions ----------
+--------------------------------------
+-- Create KeyMap Options (kmo)
 local function kmo(desc)
   return { noremap = true, silent = true, desc = desc }
 end
 
+
+---------------------------
+---------- Setup ----------
+---------------------------
+fzf_lua.setup({
+  "borderless",
+  keymap = {
+    builtin     = fzf_config.keymap_builtin,
+    fzf         = fzf_config.keymap_fzf,
+  },
+  winopts       = fzf_config.window_options,
+  git           = fzf_config.git_config,
+  actions = {
+    files       = fzf_config.files_actions,
+    buffers     = fzf_config.buffers_actions,
+  },
+  lsp = {
+    symbols = fzf_config.lsp_document_symbols_config,
+  },
+  oldfiles      = fzf_config.oldfiles_config,
+  grep          = fzf_config.grep_config,
+  buffers       = fzf_config.buffers_config,
+  tabs          = fzf_config.tabs_config,
+  files         = fzf_config.files_config,
+  -- fzf_colors    = fzf_config.colorschemes_config.standard,
+  fzf_opts      = fzf_config.global_fzf_opts,
+  previewers    = fzf_config.fzf_previewers_config,
+  defaults      = fzf_config.fzf_defaults_config,
+})
+
+
+------------------------------
+---------- UI Setup ----------
+------------------------------
+fzf_lua.register_ui_select(function(_, items)
+  -- use `fzf-lua` for replace vim.ui.select 
+  -- https://github.com/ibhagwan/fzf-lua/wiki#ui-select-auto-size
+  local min_h, max_h = 0.15, 0.70
+  local h = (#items + 4) / vim.o.lines
+  if h < min_h then
+    h = min_h
+  elseif h > max_h then
+    h = max_h
+  end
+  return { winopts = { height = h, width = 0.60, row = 0.40 } }
+end)
+
+
+----------------------------------
+---------- Key Mappings ----------
+----------------------------------
+
+----- Control Modifier
+----- Mappings: C-p, C-x, C-, , C-., C-]
 -- vim.keymap.set("n", "<C-P>", function() require('fzf-lua').live_grep_resume() end, kmo("Search"))
 vim.keymap.set("n", "<C-p>", function() require('fzf-lua').live_grep() end, kmo("Search"))
 vim.keymap.set("n", "<C-,>", function() require('fzf-lua').files({ resume = false }) end, kmo("Files"))
@@ -114,8 +82,28 @@ vim.keymap.set("n", "<C-.>", function() require('fzf-lua').oldfiles() end, kmo("
 vim.keymap.set("n", "<C-]>", function() require('fzf-lua').buffers() end, kmo("Buffers"))
 -- vim.keymap.set("n", "<C-/>", function() require('fzf-lua').lsp_document_symbols() end, kmo("LSP Document Symbols"))
 -- vim.keymap.set("n", "<C-'>", function() require('fzf-lua').lsp_live_workspace_symbols() end, kmo("Live Workspace Symbols"))
+-- vim.keymap.set({ "i" }, "<C-x><C-f>",
+--   function()
+--     require("fzf-lua").complete_file({
+--       cmd = "rg --files",
+--       winopts = { preview = { hidden = "nohidden" } }
+--     })
+--   end, { silent = true, desc = "Fuzzy complete file" })
+
+vim.keymap.set({ "n", "v", "i" }, "<C-x><C-f>", function() require("fzf-lua").complete_path() end, { silent = true, desc = "Fuzzy complete path" })
+
+
+----- Control + Shift Modifier
 vim.keymap.set("n", "<C-S-D>", function() require('fzf-lua').lsp_document_symbols() end, kmo("LSP Document Symbols"))
 vim.keymap.set("n", "<C-S-W>", function() require('fzf-lua').lsp_live_workspace_symbols() end, kmo("Live Workspace Symbols"))
+-- Misc
+vim.keymap.set("n", "<C-S-K>", function() require('fzf-lua').commands() end, kmo("Commands"))
+vim.keymap.set("n", "<C-S-P>", function() require('fzf-lua').resume() end, kmo("Resume"))
+vim.keymap.set("n", "<C-S-G>", function() require('fzf-lua').grep_project() end, kmo("Grep Project"))
+vim.keymap.set("n", "<C-S-T>", function() require('fzf-lua').tabs() end, kmo("Tabs"))
+
+
+----- Leader
 -- <leader>f
 vim.keymap.set("n", "<leader>fb", function() require('fzf-lua').buffers() end, kmo("Buffers"))
 vim.keymap.set("n", "<leader>ft", function() require('fzf-lua').tabs() end, kmo("Tabs"))
@@ -158,18 +146,4 @@ vim.keymap.set("n", "<leader>fla", function() require('fzf-lua').lsp_code_action
 vim.keymap.set("n", "<leader>flf", function() require('fzf-lua').lsp_finder() end, kmo("LSP Finder"))
 vim.keymap.set("n", "<leader>flx", function() require('fzf-lua').diagnostics_document() end, kmo("Document Diagnostics"))
 vim.keymap.set("n", "<leader>flX", function() require('fzf-lua').diagnostics_workspace() end, kmo("Workspace Diagnostics"))
--- Misc
-vim.keymap.set("n", "<C-S-K>", function() require('fzf-lua').commands() end, kmo("Commands"))
-vim.keymap.set("n", "<C-S-P>", function() require('fzf-lua').resume() end, kmo("Resume"))
-vim.keymap.set("n", "<C-S-G>", function() require('fzf-lua').grep_project() end, kmo("Grep Project"))
-vim.keymap.set("n", "<C-S-T>", function() require('fzf-lua').tabs() end, kmo("Tabs"))
 
--- vim.keymap.set({ "i" }, "<C-x><C-f>",
---   function()
---     require("fzf-lua").complete_file({
---       cmd = "rg --files",
---       winopts = { preview = { hidden = "nohidden" } }
---     })
---   end, { silent = true, desc = "Fuzzy complete file" })
-
-vim.keymap.set({ "n", "v", "i" }, "<C-x><C-f>", function() require("fzf-lua").complete_path() end, { silent = true, desc = "Fuzzy complete path" })
